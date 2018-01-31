ba37f750241929088f21f7a81bc20ad4c4181219
AmeyVM <mv.amey15@gmail.com>
2018-01-31 08:50:39 +0530

    Queue code array implimetation with all neccessary funtionalities added

--- Queue.c
+++ Queue.c
@@ -17,34 +17,81 @@ Queue* createQueue(int capacity)
 	queue -> front = 0;
 	queue -> size  = 0;
 	queue -> rear  = -1;
+	queue -> capacity = capacity;
 	queue -> array = (int*) malloc(queue -> capacity * sizeof(int));
-	printf("\n Queue of capcaity %d is created.", capacity);
+	printf("\n Queue of capcaity %d is created.", queue -> capacity);
 	
 	return queue;
 
 }
 
+int isFull(Queue* queue)
+{
+	if (queue -> size == queue -> capacity)
+	{
+		printf("\n Queue is Full");
+		return 1;
+	}
 
+	return 0;
+
+}
+
+
+int isEmpty(Queue* queue)
+{	
+	if (queue -> size == 0)
+	{
+		printf("\n Queue is Empty");
+		return 1;
+	}
+	return 0;
+	
+}
 void enqueue(Queue* queue, int data)
 {
-//	Queue* queue =  queue;//(Queue*) malloc(sizeof(Queue));
 
-	printf("\n Queue Rear is %d ", queue -> rear);
+	if (isFull(queue) == 1)
+		return;
 	++queue -> rear;
 	++queue -> size;
-	printf("\n Queue Rear is %d ", queue -> rear);	
 	queue -> array[queue -> rear] = data;
 	printf("\n %d is Enqueued to the Queue.",data);
 	
+}
+
+
+int dequeue(Queue* queue)
+{
+	int data;
+	
+	if (isEmpty(queue) == 1)
+		return 0;
+	data = queue ->array[queue -> front];
+	++queue -> front;
+	--queue -> size;
+
+	return data;
 
 }
 
 int main()
 {
-	Queue* queue = (Queue*) malloc(sizeof(Queue));
-	
-	createQueue(5);
+	Queue* queue = createQueue(3);
 	
 	enqueue(queue, 23);
+	enqueue(queue, 56);
+	enqueue(queue, 78);
+	enqueue(queue, 93);
+
+	printf("\n Current size of Queue is %d\n", queue -> size);
 		
+	printf("\n The latest element dequeued is %d ", dequeue(queue));
+	printf("\n The latest element dequeued is %d ", dequeue(queue));
+	printf("\n The latest element dequeued is %d ", dequeue(queue));
+	printf("\n The latest element dequeued is %d ", dequeue(queue));
+	
+	printf("\n Current size of Queue is %d\n", queue -> size);
+
+
 }
